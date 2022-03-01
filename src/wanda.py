39d03b91f7e9b54b4dad79ac9fd45751112006f9
Wanda Carlson <87515018+wanda-carlson@users.noreply.github.com>
2022-03-01 12:33:34 +0400

    notifications 0.3
    notifications 0.3

--- src/wanda.py
+++ src/wanda.py
@@ -4,3 +4,4 @@ print("Wanda testing v0.8.0") # local
 print("Wanda testing v0.8.0 Preflight 0.1")
 print("Wanda testing notifications 0.2")
 print("multispace testing 0.8.1")
+print("Wanda testing notifications 0.3")
