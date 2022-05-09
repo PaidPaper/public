b86f6cd117da4c4370645dbfb85829c699651cee
Henry R <87513666+henry-richard@users.noreply.github.com>
2022-05-09 14:58:27 +0400

    Starting over Part 1

--- src/main.py
+++ src/main.py
@@ -6,27 +6,4 @@ if __name__ == "__main__":
     print("Hello world 1")
     print("Hello world 2")
     print("Hello world 3")
-This is a comment
-This is another comment
-This is yet another comment
-And yet another comment
-Aaaand yet another comment
-This should work
-Ok last one
-Doesn't work
-Retry 1
-Retry 2
-Retry 3
-Retry 4
-Retry 5
-
-Reviewer & Assignee 1
-Bot Review 1
-- commit 1 
-- commit 2
-Bot Review 2
-Bot Review 3
-Bot Review 4
-Bot Review 5
-Bot Review 1 - Redo
-Bot Review 2 - Redo
+Starting over Part 1
