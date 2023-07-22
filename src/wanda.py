193da966feba48fe2e3bbe4230db15688c1c9f9e
Wanda Carlson <87515018+wanda-carlson@users.noreply.github.com>
2023-07-22 23:39:58 +0400

    mono repo 4.1 path testing

--- src/wanda.py
+++ src/wanda.py
@@ -5,5 +5,6 @@ print("Wanda testing v0.8.0") # local
 print("Wanda testing v0.8.0 Preflight 0.1")
 print("Wanda testing notifications 0.2") # old local testing
 print("Wanda testing preflight 0.8.1")
+print("Mono repo path Sat 22 Jul")
 
 > Core Testing: Sun 10 Jul 23, 1:22AM MUSCAT
