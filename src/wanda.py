cbbe41dae03be49eca6085b59a026ffdea61be5b
Wanda Carlson <87515018+wanda-carlson@users.noreply.github.com>
2023-08-23 11:47:01 +0500

    Update wanda.py

--- src/wanda.py
+++ src/wanda.py
@@ -5,3 +5,11 @@ print("Wanda testing v0.8.0") # local
 print("Wanda testing v0.8.0 Preflight 0.1")
 print("Wanda testing notifications 0.2") # old local testing
 print("Wanda testing preflight 0.8.1")
+print("Wanda testing v0.8.0 Preflight 0.1")
+print("Wanda testing notifications 0.2") # old local testing
+print("Wanda testing preflight 0.8.1")
+
+print("Wanda testing v0.8.0 Preflight 0.1")
+print("Wanda testing notifications 0.2") # old local testing
+print("Wanda testing preflight 0.8.1")
+
