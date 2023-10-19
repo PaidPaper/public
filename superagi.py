0034050bb99da9e3be678b0c4d94b39aa3f6811a
Henry R <87513666+henry-richard@users.noreply.github.com>
2023-10-19 21:06:48 +0500

    add arrays method

--- superagi.py
+++ superagi.py
@@ -1,1 +1,11 @@
-superAGI
+def add_arrays(arr1, arr2):
+    if len(arr1) != len(arr2):
+        raise ValueError("Arrays must have the same length for addition")
+
+    result = []
+
+    # Iterate through the arrays and add corresponding elements
+    for i in range(len(arr1)):
+        result.append(arr1[i] - arr2[i])
+
+    return result
